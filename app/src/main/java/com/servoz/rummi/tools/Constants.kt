3947c824ee067441ead8e9d8279f0c7e913e2668
Oscar Ocampo <oz@oz-laptop.localdomain>
2020-07-05 18:55:11 -0500

    Exclude Contants from Git

--- app/src/main/java/com/servoz/rummi/tools/Constants.kt
+++ app/src/main/java/com/servoz/rummi/tools/Constants.kt
@@ -4,7 +4,7 @@ package com.servoz.rummi.tools
 const val URL ="https://rummi.servoz.tk"
 const val API_URL ="$URL/API/app/fetch/"
 //change this to an unique string
-const val API_KEY="b&w?2jjpiQN4qKiQoazOOdo9hkJ5GRA8a?LZbFgG_va2/X7/Xt_"
+const val API_KEY="b&w?2jjpiQN4qKiQoazOOdoH_iVQ0HKvXY_Q9hkJ5GRA8a?LZbFgG_va2/X7/Xt_"
 const val TIMEZONE="America/Bogota"
 const val PREF_FILE="com.servoz.rummi.prefs"
 const val DB_NAME="rummi.db"
